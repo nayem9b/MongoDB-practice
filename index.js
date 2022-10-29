b8830b106b9ccca81d30bb0c68b0c5b59dabd9df
nayem9b <razwanniam1@gmail.com>
2022-10-30 04:32:30 +0600

    Post method checked

--- index.js
+++ index.js
@@ -2,44 +2,40 @@ const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const cors = require("cors");
-const port = process.env.local || 5000;
-const users = [
-  { id: 1, name: "Sabana", email: "sabana@gmail.com" },
-  { id: 2, name: "Sabnoor", email: "sabnoor@gmail.com" },
-  { id: 3, name: "Sabila", email: "sabila@gmail.com" },
-];
+const port = process.env.PORT || 5000;
+
 app.use(cors());
+app.use(express.json());
+
+// user:nayem
+// Password:SqvH0NNaQWRXeEJl
+
+const uri =
+  "mongodb+srv://nayem:SqvH0NNaQWRXeEJl@cluster0.dafmrk2.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
-mongodb+srv://nayem:<password>@cluster0.dafmrk2.mongodb.net/?retryWrites=true&w=majority
 async function run() {
   try {
-    const database = client.db("insertDB");
-    const haiku = database.collection("haiku");
-    // create a document to insert
-    const doc = {
-      title: "Record of a Shriveled Datum",
-      content: "No bytes, no problem. Just insert a document, in MongoDB",
-    };
-    const result = await haiku.insertOne(doc);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    const usersCollection = client.db("insertDB").database.collection("users");
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = usersCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
   } finally {
-    await client.close();
   }
 }
-run().catch(console.dir);
+run().catch((err) => console.log(err));
 app.get("/", (req, res) => {
   res.send("This is my homepage");
 });
-app.get("/users", async (req, res) => {
-  const cursor = userCollection.find({});
-  const users = await cursor.toArray();
-  res.send(users);
-});
+
 app.listen(port, () => {
   console.log("my app is running on", port);
 });
